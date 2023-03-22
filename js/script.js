a072a4ccc83beec8889d3ae9cccc30e43fbefa25
samuelebumbello <samuelebumbello17@gmail.com>
2023-03-22 17:41:26 +0100

    [added] constants for multiples

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
+
+// variabile per aggiungere il valore
+let value = "";
+
 for (let i = 1; i < 101; i++) {
-    console.log('Programma' , i);
-    
+    value = i;
+
+// dichiaro la costante con i multipli di 3 dando un valore "Fizz"
+    if(!(i % 3)){
+        value = 'Fizz';
+    }
+// dichiaro la costante con i multipli di 5 dando un valore "Buzz"
+    if(!(i % 5)){
+        value = 'Buzz';
+    }
+// dichiaro la costante con i multipli di 3 e 5 dando un valore "FizzBuzz"
+    if(!(i % 3) && !(i % 5)) {
+        value = 'FizzBuzz';
+    }
+    console.log(value);
 }
 
